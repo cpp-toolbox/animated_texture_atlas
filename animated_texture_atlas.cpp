711656230337c43435d41ac32d561bf16a116749
cuppajoeman <ccn@cuppajoeman.com>
2024-12-08 05:33:49 -0500

    feat: add support for texture packer

--- animated_texture_atlas.cpp
+++ animated_texture_atlas.cpp
@@ -6,19 +6,26 @@
 
 using json = nlohmann::json;
 
-AnimatedTextureAtlas::AnimatedTextureAtlas(const std::string &spritesheet_json_path,
-                                           const std::string &spritesheet_path, double ms_per_sprite)
-    : texture_atlas{spritesheet_json_path, spritesheet_path, true, true}, ms_per_sprite{ms_per_sprite},
-      ms_prev_time{0.0}, ms_accumulated_time{0.0}, curr_sprite{0} {
-    std::ifstream file(spritesheet_json_path);
-    json spritesheet_json;
-    file >> spritesheet_json;
+AnimatedTextureAtlas::AnimatedTextureAtlas(const std::string &json_path, const std::string &animated_texture_atlas_path,
+                                           double ms_per_animation_frame, std::optional<TexturePacker> texture_packer)
+    : texture_atlas{json_path, animated_texture_atlas_path, true, true},
+      animated_texture_atlas_path(animated_texture_atlas_path), texture_packer(texture_packer),
+      ms_per_animation_frame{ms_per_animation_frame}, ms_prev_time{0.0}, ms_accumulated_time{0.0},
+      curr_animation_frame{0} {
 
-    auto sprites = spritesheet_json["sub_textures"];
-    total_sprites = sprites.size();
+    if (this->texture_packer.has_value()) {
+        /*auto sprites = texture_packer.*/
+        total_animation_frames = texture_packer->get_atlas_size_of_sub_texture(animated_texture_atlas_path);
+    } else {
+        std::ifstream file(json_path);
+        json spritesheet_json;
+        file >> spritesheet_json;
+        auto sprites = spritesheet_json["sub_textures"];
+        total_animation_frames = sprites.size();
+    }
 }
 
-std::vector<glm::vec2> AnimatedTextureAtlas::get_texture_coordinates_of_sprite(double ms_curr_time) {
+std::vector<glm::vec2> AnimatedTextureAtlas::get_texture_coordinates_of_current_animation_frame(double ms_curr_time) {
     if (ms_prev_time == 0.0) {
         // animation hasn't run yet
         ms_prev_time = ms_curr_time;
@@ -27,10 +34,21 @@ std::vector<glm::vec2> AnimatedTextureAtlas::get_texture_coordinates_of_sprite(d
     ms_accumulated_time += ms_curr_time - ms_prev_time;
     ms_prev_time = ms_curr_time;
 
-    while (ms_accumulated_time >= ms_per_sprite) {
-        curr_sprite = (curr_sprite + 1) % total_sprites;
-        ms_accumulated_time -= ms_per_sprite;
+    while (ms_accumulated_time >= ms_per_animation_frame) {
+        curr_animation_frame = (curr_animation_frame + 1) % total_animation_frames;
+        ms_accumulated_time -= ms_per_animation_frame;
     }
 
-    return texture_atlas.get_texture_coordinates_of_sub_texture(std::to_string(curr_sprite));
+    std::string current_animation_frame = std::to_string(curr_animation_frame);
+
+    std::vector<glm::vec2> texture_coords;
+    if (this->texture_packer.has_value()) {
+        auto val =
+            texture_packer->get_packed_texture_sub_texture_atlas(animated_texture_atlas_path, current_animation_frame);
+        texture_coords = val.texture_coordinates;
+    } else {
+        texture_coords = texture_atlas.get_texture_coordinates_of_sub_texture(current_animation_frame);
+    }
+
+    return texture_coords;
 }
--- animated_texture_atlas.hpp
+++ animated_texture_atlas.hpp
@@ -6,22 +6,28 @@
 #include <vector>
 #include <glm/glm.hpp>
 #include <nlohmann/json.hpp>
+#include <optional>
 
 #include "sbpt_generated_includes.hpp"
 
 class AnimatedTextureAtlas {
   public:
-    AnimatedTextureAtlas(const std::string &json_path, const std::string &spritesheet_path, double ms_per_sprite);
+    AnimatedTextureAtlas(const std::string &json_path, const std::string &animated_texture_atlas_path,
+                         double ms_per_animation_frame, std::optional<TexturePacker> texture_packer = std::nullopt);
 
-    std::vector<glm::vec2> get_texture_coordinates_of_sprite(double ms_curr_time);
+    std::vector<glm::vec2> get_texture_coordinates_of_current_animation_frame(double ms_curr_time);
 
   private:
     TextureAtlas texture_atlas;
-    double ms_per_sprite;
+    // note that if you have the texture_packer, then we don't use the texture_atlas, it has a superset of the atlases
+    // functionality
+    std::optional<TexturePacker> texture_packer;
+    std::string animated_texture_atlas_path;
+    double ms_per_animation_frame;
     double ms_prev_time;
     double ms_accumulated_time;
-    unsigned int curr_sprite;
-    unsigned int total_sprites;
+    unsigned int curr_animation_frame;
+    unsigned int total_animation_frames;
 };
 
 #endif // ANIMATED_TEXTURE_ATLAS_HPP
